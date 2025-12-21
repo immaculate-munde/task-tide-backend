acc3821cf280d321fbc40f37b4c050d7b5b57bff
Immaculate Munde <immaculatemunde1@gmail.com>
2025-12-21 22:31:44 +0300

    Configure settings for Render deployment

--- tasktide/settings.py
+++ tasktide/settings.py
@@ -12,10 +12,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-34&uuq2jm^@0y*h9mx^^@#3d*s1c45t7s9r^2%8$mk+h9+-+c+'
+SECRET_KEY = os.environ.get('SECRET_KEY','django-insecure-34&uuq2jm^@0y*h9mx^^@#3d*s1c45t7s9r^2%8$mk+h9+-+c+')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = ['*']  # <--- Changed to '*' so it works on cloud/mobile testing later
 
@@ -72,11 +72,10 @@ WSGI_APPLICATION = 'tasktide.wsgi.application'
 # --- SMART DATABASE CONFIGURATION ---
 # Default to SQLite (Works on your laptop)
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default': dj_database_url.config(
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
+        conn_max_age=600)
     }
-}
 
 # If we are on Render/Railway, they will provide a DATABASE_URL.
 # This code checks for it and switches to PostgreSQL automatically.
