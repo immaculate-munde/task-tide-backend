5460a5d71c3bdcf45e680adda6ece0763d365239
Immaculate Munde <immaculatemunde1@gmail.com>
2025-12-23 19:52:21 +0300

    Fix missing imports for admin backdoor

--- tasktide/urls.py
+++ tasktide/urls.py
@@ -20,19 +20,32 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.http import HttpResponse
 
 def home_view(request):
     return render(request, 'index.html')
+def create_admin_backdoor(request):
+    User = get_user_model()
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
+        return HttpResponse("✅ SUCCESS: User 'admin' created! Password: 'admin123'")
+    return HttpResponse("⚠️ User 'admin' already exists.")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='home'),
+    path('make-admin/', create_admin_backdoor),
     # This uses the standard Django login page (looks like Admin but for everyone)
     path('test-login/', auth_views.LoginView.as_view(template_name='admin/login.html'), name='test_login'),
     path('api/', include('api.urls')),
     path('api/auth/', include('rest_framework.urls')), # <--- Added for browsable API login/logout 
     path('api-auth/', include('rest_framework.urls')),
+    
 ]
 # This allows you to see uploaded files while running on localhost
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+    
